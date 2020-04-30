34886d1b47925e31de475aeb8b66d8fef77abcdc
Tamas Cservenak <tamas@cservenak.net>
2020-04-30 17:02:47 +0200

    Tidy

--- src/main/java/org/cstamas/test/App.groovy
+++ src/main/java/org/cstamas/test/App.groovy
@@ -1,4 +1,4 @@
-package org.cstamas.test;
+package org.cstamas.test
 
 import java.io.FileInputStream
 import java.io.IOException
@@ -8,6 +8,7 @@ class App
     public static void main( String[] args ) throws IOException
     {
         try (FileInputStream fis = new FileInputStream('test')) {
+          // whatever, as this does not compile
         }
     }
 }
